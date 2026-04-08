f08b8e5097e46eabf5b7a4b98c1f8324dd45a0b9
mtee <mtee#outlook.com>
2026-04-08 11:30:34 +0800

    d4c

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 print("hello world!")
 print("This is a test file.")
-def add(a, b):
-    return a + b
+def add(a, c):
+    return a + c
 
 result = add(5, 3)
 print(f"The sum of 5 and 3 is: {result}")
